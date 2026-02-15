a79d055056e81efe714d1d4a5c83f3b02634d697
192521325simats-code <192521325.simats@saveetha.com>
2026-02-15 18:33:24 +0530

    Add files via upload

--- code1.c
+++ code1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main()
+int main ()
 {
-printf ("Hello world");
+int a,b,sum;
+printf("enter integer:");
+scanf("%d",&a);
+printf("enter integer:");
+scanf("%d",&b);
+sum = a+b;
+printf("sum=%d",sum);
 return 0;
 }
