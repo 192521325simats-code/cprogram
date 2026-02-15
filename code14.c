903a743e880dc9a3519240769dcacc2c096a8252
192521325simats-code <192521325.simats@saveetha.com>
2026-02-15 21:53:01 +0530

    Add files via upload

---
+++ code14.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+ float celsius,F;
+ printf("enter celsius:");
+ scanf("%f",&celsius);
+ F=(celsius*9/5)+32;
+ printf("celsius into fahrenheit=%.2f",F);
+ return 0;
+}
